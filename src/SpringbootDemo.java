a2c94825767de6c14f03fd7fa400ebd8c2230179
liaozhifei <liaozhifei0523@163.com>
2019-05-28 16:21:06 +0800

    测试提交2222

--- src/SpringbootDemo.java
+++ src/SpringbootDemo.java
@@ -1,8 +1,8 @@
 
 public class SpringbootDemo {
-public static void main(String[] args) {
-	System.out.println("hello");
-	System.out.println("hello");
-	System.out.println("hello");
-}
+    public static void main(String[] args) {
+
+
+
+    }
 }
